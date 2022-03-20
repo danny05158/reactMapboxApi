243d344f6265395ddbdca6bd354ebb03c9d95386
Daniel Casasola <casasolad86@gmail.com>
2022-03-20 15:54:28 -0500

    responsive nav var

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,23 @@ const Navbar = (props) => {
   }
 
   return (
-      <div className="nav-mixed menu">
-          <div>
-              <h3>Display Data</h3>
-                  <div>
-                    <span>Primary</span>
-                  </div>
-              <h3>Data</h3>
-                <button
-                  onClick={() => handleClick()}>
-                  {props.state.displayData ? 'Hide' : 'Show'}
-                </button>
-          </div>
+    <div className="nav-mixed">
+      <div className="nav-mixed-items">
+        <h3>Display Data</h3>
       </div>
-  )
+      <div className="nav-mixed-items">
+        <span>Primary</span>
+      </div>
+      <div className="nav-mixed-items">
+        <h3>Data</h3>
+      </div>
+      <div className="nav-mixed-items">
+        <button onClick={() => handleClick()}>
+          {props.state.displayData ? "Hide" : "Show"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
